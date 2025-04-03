12aceb2a0c6ac7d14fc1c24c632344af844210fe
VictorGBroxia <victor.gonzalez@broxia-int.com>
2025-04-03 08:33:36 -0600

    feat: added bronze data dir.

--- utils/config.py
+++ utils/config.py
@@ -5,3 +5,6 @@ ROOT_DIR = Path(__file__).resolve().parent.parent
 
 # Directory of the raw data
 RAW_DATA_DIR = ROOT_DIR / "data" / "raw"
+
+# Directory of data bases
+BRONZE_DATA_DIR = ROOT_DIR / "data" / "bronze"

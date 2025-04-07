d4f5ac7440f6be3b594076e59c1c370196a97a33
VictorGBroxia <victor.gonzalez@broxia-int.com>
2025-04-07 01:08:08 -0600

    refactor: changed the Variables nane in the class Paths.

---
+++ src/utils/config.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Paths:
+    # Root directory of the project
+    PROJECT_ROOT = Path(__file__).parents[2]  # Two levels up from this file
+    # Directory of the raw data
+    RAW_DATA = PROJECT_ROOT / "data" / "raw"
+    # Directory of the processed data
+    PROCESSED_DATA = PROJECT_ROOT / "data" / "processed"
+    # Directory of  bronze data base
+    BRONZE_DB_URL = os.getenv("BRONZE_DB_URL", "postgresql://user:password@localhost:5432/tu_base")

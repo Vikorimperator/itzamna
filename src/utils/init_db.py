686892d4378db5bcc3d46a3e4f6681689c60053e
VictorGBroxia <victor.gonzalez@broxia-int.com>
2025-04-08 00:07:31 -0600

    refactor: init_bronze schema now have loggings.

--- src/utils/init_db.py
+++ src/utils/init_db.py
@@ -1,15 +1,21 @@
 from sqlalchemy import create_engine, text
 from utils.config import Paths
 from pathlib import Path
+import logging
 
 def init_broze_schema():
     engine =  create_engine(Paths.BRONZE_DB_URL)
     schema_path = Path(Paths.PROJECT_ROOT) / 'database' / 'schema_bronze.sql'
     
-    with engine.begin() as connection:
-        with open(schema_path, 'r') as file:
-            sql_statements = file.read()
-            # Execute the SQL commands to create the schema
-            connection.execute(text(sql_statements))
-            
-    print("✅ Tablas Bronce creadas correctamente.")
+    logging.info("🔧 Executing schema_bronze.sql...")
+    
+    try:
+        with engine.begin() as connection:
+            with open(schema_path, 'r') as file:
+                sql_statements = file.read()
+                # Execute the SQL commands to create the schema
+                connection.execute(text(sql_statements))
+        logging.info("✅ Tables created or verified.")
+    except Exception as e:
+        logging.exception(f"❌ Error creating tables: {e}")
+        raise

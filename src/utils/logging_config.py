05df4e131e1e6c6c59f17b812fd5eb9d83fb6aac
VictorGBroxia <victor.gonzalez@broxia-int.com>
2025-04-08 00:17:46 -0600

    reafactor: function setup_logging now creates a .log file in the root of the project.

--- src/utils/logging_config.py
+++ src/utils/logging_config.py
@@ -1,7 +1,15 @@
 import logging
+from pathlib import Path
+from utils.config import Paths
 
 def setup_logging(level=logging.INFO):
+    log_path = Paths.PROJECT_ROOT / "pipeline.log"
+
     logging.basicConfig(
         level=level,
-        format="%(asctime)s - %(levelname)s - %(message)s"
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        handlers=[
+            logging.FileHandler(log_path),
+            logging.StreamHandler()
+        ]
     )

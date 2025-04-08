48e2b5ce6d56de57b6091a53fade908d6651fa7f
VictorGBroxia <victor.gonzalez@broxia-int.com>
2025-04-07 23:52:43 -0600

    refactor: using loggin from template.

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,6 +1,19 @@
 from src.utils.init_db import init_bronze_schema
 from src.data_ingestion.load_csv import auto_discover_and_ingest
+from src.utils.logging_config import setup_logging
+import logging
+
+setup_logging()
 
 if __name__ == "__main__":
-    init_bronze_schema()             # Crea tablas si no existen
-    auto_discover_and_ingest()       # Ingresa los CSV automáticamente
+    logging.info("🚀 Starting pipeline...")
+    
+    try:
+        init_bronze_schema()        # Crea tablas si no existen
+        logging.info("✅ Schema initialized.")
+
+        auto_discover_and_ingest()  # Ingresa los CSV automáticamente
+        logging.info("✅ Ingestion completed successfully.")
+
+    except Exception as e:
+        logging.error(f"❌ Error in pipeline execution: {e}")

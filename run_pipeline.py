8a3fae0d529739de5ff15fa684ebef6055197a5f
VictorGBroxia <victor.gonzalez@broxia-int.com>
2025-04-21 23:23:38 -0600

    feat: Added initialization of the silver schema and the use of the process_all_bronze_data, load_lecturas_silver and load_sensor_catalog functions to transform the data and load it into the silver database.

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,19 +1,28 @@
-from src.utils.init_db import init_bronze_schema
+from src.utils.init_db import init_bronze_schema, init_silver_schema
 from src.data_ingestion.load_csv import auto_discover_and_ingest
 from src.utils.logging_config import setup_logging
+from src.data_transformation.transform_to_silver import process_all_bronze_data
+from src.data_loading.load_to_silver import load_lecturas_silver, load_sensor_catalog
 import logging
 
 setup_logging()
 
 if __name__ == "__main__":
     logging.info("Starting pipeline...")
-    
+
     try:
-        init_bronze_schema()        # Crea tablas si no existen
-        logging.info("Schema initialized.")
+        init_bronze_schema()        # Crea tablas bronce si no existen
+        init_silver_schema()        # Crea tablas silver si no existen
+        logging.info("Schemas initialized.")
 
         auto_discover_and_ingest()  # Ingresa los CSV automáticamente
-        logging.info("Ingestion completed successfully.")
+        logging.info("Ingestion to bronze completed successfully.")
+
+        # Transformación y carga a Silver
+        lecturas_df, catalog_df = process_all_bronze_data()
+        load_lecturas_silver(lecturas_df)
+        load_sensor_catalog(catalog_df)
+        logging.info("Transformation and loading to silver completed successfully.")
 
     except Exception as e:
         logging.exception(f"Error in pipeline execution: {e}")

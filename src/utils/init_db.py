ae3a80eba99d3ad55e2cf65bcddf6c5dc9f89601
VictorGBroxia <victor.gonzalez@broxia-int.com>
2025-04-21 23:19:10 -0600

    feat: Added the init_silver_schema function.

--- src/utils/init_db.py
+++ src/utils/init_db.py
@@ -19,3 +19,18 @@ def init_bronze_schema():
     except Exception as e:
         logging.exception(f"Error creating tables: {e}")
         raise
+    
+def init_silver_schema():
+    engine = create_engine(Paths.SILVER_DB_URL)
+    schema_path = Path(Paths.PROJECT_ROOT) / 'database' / 'schema_silver.sql'
+
+    logging.info("Executing schema_silver.sql...")
+    try:
+        with engine.begin() as connection:
+            with open(schema_path, 'r') as file:
+                sql_statements = file.read()
+                connection.execute(text(sql_statements))
+        logging.info("Silver tables created or verified.")
+    except Exception as e:
+        logging.exception(f"Error creating silver tables: {e}")
+        raise

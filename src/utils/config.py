b93edea311bba324cbda93d612d7af534ece7825
VictorGBroxia <victor.gonzalez@broxia-int.com>
2025-04-27 23:51:03 -0600

    refeactor: adding .resolve to PROJECT_ROOT.

--- src/utils/config.py
+++ src/utils/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 class Paths:
     # Root directory of the project
-    PROJECT_ROOT    = Path(__file__).parents[2]
+    PROJECT_ROOT = Path(__file__).parents[2].resolve()
     # Directory of the raw data
     RAW_DATA        = PROJECT_ROOT / "data" / "raw"
     # DuckDB catalog file

6d6714159166149215ace7a5327f04774b7ff2a5
VictorGBroxia <victor.gonzalez@broxia-int.com>
2025-04-27 23:32:00 -0600

    feat: We create the ingested_files table.

--- src/data_ingestion/setup_lake.py
+++ src/data_ingestion/setup_lake.py
@@ -5,9 +5,17 @@ def init_lakehouse():
     # Conecta al archivo; lo crea si no existe
     con = duckdb.connect(Paths.LAKE_FILE)
     
-    # Crea los 3 esquemas lógicos
+    # Crea los 2 esquemas lógicos
     con.execute("CREATE SCHEMA IF NOT EXISTS bronze;")
     con.execute("CREATE SCHEMA IF NOT EXISTS silver;")
+    
+    # Crea la tabla de archivos ingestados
+    con.execute("""CREATE TABLE IF NOT EXISTS bronze.ingested_files (
+        file_name TEXT PRIMARY KEY,
+        ingestion_timestamp TIMESTAMP
+        );"""
+        )
+    
     con.close()
 
 if __name__ == "__main__":

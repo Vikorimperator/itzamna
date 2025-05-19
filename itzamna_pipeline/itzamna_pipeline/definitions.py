4d604a7bb173c7065f36a5f9fa881e2e1450e98d
VictorGBroxia <victor.gonzalez@broxia-int.com>
2025-05-19 14:19:38 -0600

    feat: added a scjhedule for the daily_ingestion to bronze.

--- itzamna_pipeline/itzamna_pipeline/definitions.py
+++ itzamna_pipeline/itzamna_pipeline/definitions.py
@@ -1,7 +1,8 @@
 from dagster import Definitions
-
 from itzamna_pipeline.jobs import ingest_all_bronze_op_based
+from itzamna_pipeline.schedules import daily_ingestion_schedule
 
 defs = Definitions(
-    jobs=[ingest_all_bronze_op_based]
+    jobs=[ingest_all_bronze_op_based],
+    schedules=[daily_ingestion_schedule]
 )

5b4181ba95e5a6838a5c3071d9030e4d61bd4c8f
VictorGBroxia <victor.gonzalez@broxia-int.com>
2025-05-19 13:03:12 -0600

    refactor: Added the use of In, Out and Nothing to the @op's.

--- itzamna_pipeline/itzamna_pipeline/ops_ingestion.py
+++ itzamna_pipeline/itzamna_pipeline/ops_ingestion.py
@@ -1,18 +1,18 @@
-from dagster import op
+from dagster import op, In, Out, Nothing
 from itzamna_pipeline.itzamna_core.data_ingestion.ingest_bronze import ingest_csv_to_bronze
 from itzamna_pipeline.itzamna_core.utils.config import Paths
 
-@op
+@op(out=Out)
 def ingest_sensor_data():
     for csv in Paths.RAW_DATA.glob("AYATSIL-*_all.csv"):
         ingest_csv_to_bronze(csv, table_name="sensor_data")
 
-@op
-def ingest_equipos_data():
+@op(ins={"_start": In(Nothing)}, out=Out)
+def ingest_equipos_data(_start):
     for csv in Paths.RAW_DATA.glob("BEC_AYATSIL-*_all.csv"):
         ingest_csv_to_bronze(csv, table_name="equipos")
 
-@op
-def ingest_eventos_data():
+@op(ins={"_start": In(Nothing)}, out=Out)
+def ingest_eventos_data(_start):
     for csv in Paths.RAW_DATA.glob("Eventos_AYATSIL-*_all.csv"):
         ingest_csv_to_bronze(csv, table_name="eventos")

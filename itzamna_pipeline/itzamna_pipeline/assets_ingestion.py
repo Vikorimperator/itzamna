2b9ad2fbbf459065fe76f5a1545c5ad815e0b869
VictorGBroxia <victor.gonzalez@broxia-int.com>
2025-05-18 22:07:55 -0600

    feat: The @assets sensor_data_bronze, equipment_bronze and events_bronze are added.

---
+++ itzamna_pipeline/itzamna_pipeline/assets_ingestion.py
@@ -0,0 +1,50 @@
+from dagster import asset, Output, MetadataValue
+from src.data_ingestion.ingest_bronze import ingest_csv_to_bronze
+from pathlib import Path
+
+RAW_DIR  = Path("data/raw")
+
+@asset
+def sensor_data_bronce() -> Output[None]:
+    count = 0
+    for csv in RAW_DIR.glob("AYATSIL-*_all.csv"):
+        ingest_csv_to_bronze(csv, table_name="sensor_data")
+        count += 1
+        
+    return Output(
+        value=None,
+        metadata={
+            "archivos_procesados": count,
+            "tabla": "bronze.sensor_data"
+        }
+    )
+    
+@asset
+def equipos_bronce() -> Output[None]:
+    count = 0
+    for csv in RAW_DIR.glob("BEC_AYATSIL-*_all.csv"):
+        ingest_csv_to_bronze(csv, table_name="equipos")
+        count += 1
+        
+    return Output(
+        value=None,
+        metadata={
+            "archivos_procesados": count,
+            "tabla": "bronze.equipos"
+        }
+    )
+    
+@asset
+def eventos_bronce() -> Output[None]:
+    count = 0
+    for csv in RAW_DIR.glob("Eventos_AYATSIL-*_all.csv"):
+        ingest_csv_to_bronze(csv, table_name="eventos")
+        count += 1
+        
+    return Output(
+        value=None,
+        metadata={
+            "archivos_procesados": count,
+            "tabla": "bronze.eventos"
+        }
+    )

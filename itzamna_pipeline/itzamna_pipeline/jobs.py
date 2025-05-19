a6f0bde06c48a73e61f095b3f83e9ee7c4a69e6b
VictorGBroxia <victor.gonzalez@broxia-int.com>
2025-05-19 01:19:54 -0600

    refactor: it now runs secuential jobs.

--- itzamna_pipeline/itzamna_pipeline/jobs.py
+++ itzamna_pipeline/itzamna_pipeline/jobs.py
@@ -1,12 +1,16 @@
 from dagster import job
-from itzamna_pipeline.assets_ingestion import (
-    sensor_data_bronce,
-    equipos_bronce,
-    eventos_bronce
+from itzamna_pipeline.ops_ingestion import (
+    ingest_sensor_data,
+    ingest_equipos_data,
+    ingest_eventos_data
 )
 
 @job
-def ingest_all_bronze():
-    sensor_data_bronce()
-    equipos_bronce()
-    eventos_bronce()
+def ingest_all_bronze_op_based():
+    sensor = ingest_sensor_data()
+    equipos = ingest_equipos_data()
+    eventos = ingest_eventos_data()
+
+    # Orden secuencial garantizado por el orden de llamadas
+    equipos
+    eventos

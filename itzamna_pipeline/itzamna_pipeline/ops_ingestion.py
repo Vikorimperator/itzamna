e3405598478da9d938978a8b3bd96d6c88be9909
VictorGBroxia <victor.gonzalez@broxia-int.com>
2025-05-19 13:17:59 -0600

    refactor: changed the declarition of ins in @op.

--- itzamna_pipeline/itzamna_pipeline/ops_ingestion.py
+++ itzamna_pipeline/itzamna_pipeline/ops_ingestion.py
@@ -2,17 +2,17 @@ from dagster import op, In, Out, Nothing
 from itzamna_pipeline.itzamna_core.data_ingestion.ingest_bronze import ingest_csv_to_bronze
 from itzamna_pipeline.itzamna_core.utils.config import Paths
 
-@op(out=Out)
+@op(out={"result": Out(Nothing)})
 def ingest_sensor_data():
     for csv in Paths.RAW_DATA.glob("AYATSIL-*_all.csv"):
         ingest_csv_to_bronze(csv, table_name="sensor_data")
 
-@op(ins={"_start": In(Nothing)}, out=Out)
+@op(ins={"start": In(Nothing)}, out={"result": Out(Nothing)})
 def ingest_equipos_data(_start):
     for csv in Paths.RAW_DATA.glob("BEC_AYATSIL-*_all.csv"):
         ingest_csv_to_bronze(csv, table_name="equipos")
 
-@op(ins={"_start": In(Nothing)}, out=Out)
+@op(ins={"start": In(Nothing)}, out={"result": Out(Nothing)})
 def ingest_eventos_data(_start):
     for csv in Paths.RAW_DATA.glob("Eventos_AYATSIL-*_all.csv"):
         ingest_csv_to_bronze(csv, table_name="eventos")

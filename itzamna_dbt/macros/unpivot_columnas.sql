24b6eee4a79a2235aa0270484eacebbda2ee429a
VictorGBroxia <victor.gonzalez@broxia-int.com>
2025-05-21 23:39:22 -0600

    refactor: changeg no for not in line 12.

--- itzamna_dbt/macros/unpivot_columnas.sql
+++ itzamna_dbt/macros/unpivot_columnas.sql
@@ -1,7 +1,7 @@
 {% macro generate_unpivot_sql(model, id_columns) %}
-    {#
+    {# 
         model: nombre del modelo (ej. 'lecturas_silver')
-        id_columns: lista de columnas que no se deben unpivotar 
+        id_columns: lista de columnas que NO se deben unpivotar
     #}
 
     {% set columns = adapter.get_columns_in_relation(ref(model)) %}
@@ -9,10 +9,10 @@
     
     {# Filtrar solo las columnas que no están en id_columns #}
     {% for col in columns %}
-        {% if col.name no in id_columns %}
+        {% if col.name not in id_columns %}
             {% do sensor_columns.append(col.name) %}
         {% endif %}
-    {% end for %}
+    {% endfor %}
 
     {% set selects = [] %}
     {% for sensor in sensor_columns %}

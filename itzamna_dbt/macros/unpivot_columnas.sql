14a6651847e47e773884f1660ed89edf439e8e11
VictorGBroxia <victor.gonzalez@broxia-int.com>
2025-05-21 23:22:29 -0600

    feat: added the dbt macro to get all the columns from lecturas_silver and generete the unions all.

---
+++ itzamna_dbt/macros/unpivot_columnas.sql
@@ -0,0 +1,24 @@
+{% macro generate_unpivot_sql(model, id_columns) %}
+    {#
+        model: nombre del modelo (ej. 'lecturas_silver')
+        id_columns: lista de columnas que no se deben unpivotar 
+    #}
+
+    {% set columns = adapter.get_columns_in_relation(ref(model)) %}
+    {% set sensor_columns = [] %}
+    
+    {# Filtrar solo las columnas que no están en id_columns #}
+    {% for col in columns %}
+        {% if col.name no in id_columns %}
+            {% do sensor_columns.append(col.name) %}
+        {% endif %}
+    {% end for %}
+
+    {% set selects = [] %}
+    {% for sensor in sensor_columns %}
+        {% set sql = "SELECT pozo, numero_equipo, '" ~ sensor ~ "' AS sensor, " ~ sensor ~ " AS valor FROM base" %}
+        {% do selects.append(sql) %}
+    {% endfor %}
+
+    {{ selects | join(" UNION ALL\n") }}
+{% endmacro %}

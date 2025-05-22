6b0ee84afb773c5f1dee39eb8f5282cb4c2b9a4c
VictorGBroxia <victor.gonzalez@broxia-int.com>
2025-05-21 23:28:31 -0600

    feat: added the dbt model for sensor_cobertura_real.

---
+++ itzamna_dbt/models/gold/sensor_cobertura_real.sql
@@ -0,0 +1,27 @@
+{{ config(materialized='table') }}
+
+WITH base AS (
+    SELECT * FROM {{ ref('lecturas_silver') }}
+),
+
+unpivoted AS (
+
+    {{ generate_unpivot_sql('lecturas_silver', ['pozo', 'numero_equipo']) }}
+
+),
+
+cobertura AS (
+    SELECT
+        pozo,
+        numero_equipo,
+        sensor,
+        COUNT(*) AS total_registros,
+        COUNT(valor) AS registros_validos,
+        ROUND(COUNT(valor) * 1.0 / COUNT(*), 3) AS porcentaje_valido
+    FROM unpivoted
+    GROUP BY pozo, numero_equipo, sensor
+)
+
+SELECT *
+FROM cobertura
+WHERE procentaje_valido >= 0.9
